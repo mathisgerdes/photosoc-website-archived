137b680dde7021ad9711ae2ee9d44de3e885f56a
Mathis Gerdes <MathisGerdes@gmail.com>
2018-03-17 21:24:38 +0100

    Make variable naming more consistent.
    Content used to refer to both the cache interface and individual values
    in the cache. Now call the cache interface ContentStore and refer to
    the saved values in the cache as values (i.e. data structure/string).
    
    Additionally add events to content.py.

--- flask/content.py
+++ flask/content.py
@@ -1,55 +1,67 @@
 import json
 
+
+# SPECIFIC UPDATE FUNCTIONS
+def update_events(store, events):
+    for event in events:
+        store.update(event['file'])
+    return events
+
+
 CONTENT = {
     'sites/home.json': (json.loads, None),
-    'sites/nonexistent.json': (json.loads, None),
+    'events/events.json': (json.loads, update_events)
 }
 
 TIMEOUT = 60*60*24  # time in seconds; 24h
 
-class Content(object):
+
+class ContentStore(object):
     def __init__(self, cache, urlfetch, base_url):
         self.cache = cache
         self.fetch = urlfetch
         self.base_url = base_url
 
-    def update_cache(self, name, parse_fn=None, update_fn=None):
+    def update_cache(self, fname, parse_fn=None, update_fn=None):
         """ Fetch file with given name and update cached value.
 
         Args:
-            name: File name relative to base_url (same as key in cache).
+            fname: File name relative to base_url (same as key in cache).
             parse_fn: Function to parse the plain text content of the file.
                 Default keeps the plain text.
             update_fn: Function to further process the datastructure.
                 Might invoke update/update_all again, with dynamic dependents.
         """
-        result = self.fetch(self.base_url + name)
+        result = self.fetch(self.base_url + fname)
         if result.status_code != 200:
-            content = None
-        elif parse_fn is None:
-            content = result.content
+            value = None
         else:
-            content = parse_fn(result.content)
-        self.cache.set(name, content, timeout=TIMEOUT)
+            if parse_fn is None:
+                value = result.content
+            else:
+                value = parse_fn(result.content)
+            if update_fn is not None:
+                value = update_fn(self, value)
+        self.cache.set(fname, value, timeout=TIMEOUT)
 
-        return content
+        return value
 
     def update(self, fname):
-        """ Force update of the content saved in cache for fname and dependents.
+        """ Force update of the value saved in cache for fname and dependents.
 
         Returns:
-            Updated, parsed content of fname.
+            Updated, parsed value of fname.
         """
         try:
             # update with known parse_fn and update_fn
-            content = self.update_cache(fname, *CONTENT[fname])
+            value = self.update_cache(fname, *CONTENT[fname])
         except KeyError:
             if fname.split('.')[-1].lower() == 'json':
-                content = self.update_cache(fname, json.loads)
+                value = self.update_cache(fname, json.loads)
             else:
                 # do not parse content (i.e. plain text)
-                content = self.update_cache(fname)
-        return content
+                value = self.update_cache(fname)
+        return value
 
     def update_all(self, content=CONTENT):
         """ Update all file names in content.
@@ -58,24 +70,24 @@ class Content(object):
             content: iterable giving the file names (e.g. list or dict).
         """
         values = []
-        for name in content:
-            values.append(self.update(name))
+        for fname in content:
+            values.append(self.update(fname))
         return values
 
     def __getitem__(self, name):
         """ Get item, fetch/update the value if it is not in the cache. """
-        content = self.cache.get(name)
-        if content is None:
-            content = self.update(name)
-        return content
+        value = self.cache.get(name)
+        if value is None:
+            value = self.update(name)
+        return value
 
-    def pretty_print(self, key):
-        text = json.dumps(self.cache.get(key), indent=2)
+    def _pretty_print(self, fname):
+        text = json.dumps(self.cache.get(fname), indent=2)
         return text.replace('\n', '<br>').replace(' ', '&nbsp;')
 
     def __str__(self):
-        objects = ['<h2>' + key + '</h2><p>' +
-                   self.pretty_print(key) + '</p>'
-                   for key in CONTENT]
+        values = ['<h2>' + fname + '</h2><p>' +
+                  self._pretty_print(fname) + '</p>'
+                  for fname in CONTENT]
         return ('<!doctype html><html><body><h1> Cached Values </h1>' +
-                '\n'.join(objects) + '</body></html>')
+                '\n'.join(values) + '</body></html>')
--- flask/main.py
+++ flask/main.py
@@ -1,7 +1,7 @@
 from google.appengine.api import urlfetch
 from werkzeug.contrib.cache import GAEMemcachedCache
 from flask import Flask, render_template, request
-from content import Content
+from content import ContentStore
 
 import logging
 
@@ -12,22 +12,24 @@ CONTENT_URL = 'https://mathisgerdes.github.io/photosoc-edinburgh/content/'
 app.config.from_object(__name__)
 
 cache = GAEMemcachedCache()
-content = Content(cache, urlfetch.fetch, app.config['CONTENT_URL'])
+store = ContentStore(cache, urlfetch.fetch, app.config['CONTENT_URL'])
 
 # ROUTES
 
 @app.route('/')
 def debug_root():
     try:
-        return render_template('index.html', home=content['sites/home.json'])
+        return render_template('index.html',
+                               home=store['sites/home.json'],
+                               events=store['sites/events.json'])
     except Exception as e:
         return str(e)
 
 @app.route('/admin/content')
 def show_content():
-    return str(content)
+    return str(store)
 
 @app.route('/admin/update')
 def update_data():
-    content.update_all()
-    return str(content)
+    store.update_all()
+    return str(store)
